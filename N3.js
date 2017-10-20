859751dbfb4bdd52e07b3b25488d824bacdbc4a8
Ruben Taelman <ruben.taelman@ugent.be>
2017-10-20 15:22:29 +0900

    Fix root module.exports failing in some environments

--- N3.js
+++ N3.js
@@ -1,9 +1,20 @@
 // Replace local require by a lazy loader
 var globalRequire = require;
-require = function () {};
+var exports = {};
+require = function (submodule) {
+  // Load submodules on first access
+  return exports[submodule] || Object.defineProperty(exports, submodule, {
+    configurable: true,
+    enumerable: true,
+    get: function () {
+      delete exports[submodule];
+      return exports[submodule] = globalRequire(submodule);
+    },
+  })[submodule];
+};
 
 // Expose submodules
-var exports = module.exports = {
+module.exports = {
   Lexer:        require('./lib/N3Lexer'),
   Parser:       require('./lib/N3Parser'),
   Writer:       require('./lib/N3Writer'),
@@ -12,15 +23,3 @@ var exports = module.exports = {
   StreamWriter: require('./lib/N3StreamWriter'),
   Util:         require('./lib/N3Util'),
 };
-
-// Load submodules on first access
-Object.keys(exports).forEach(function (submodule) {
-  Object.defineProperty(exports, submodule, {
-    configurable: true,
-    enumerable: true,
-    get: function () {
-      delete exports[submodule];
-      return exports[submodule] = globalRequire('./lib/N3' + submodule);
-    },
-  });
-});
